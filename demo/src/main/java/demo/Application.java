07c63bae952e6354230c06a9b9c7c3e040ce6917
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-09-21 04:35:25 -0700

    building process definition endpoint.

--- demo/src/main/java/demo/Application.java
+++ demo/src/main/java/demo/Application.java
@@ -1,8 +1,12 @@
 package demo;
 
 import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.identity.Group;
 import org.activiti.engine.identity.User;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,15 +14,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
 public class Application {
 
     @Bean
-    CommandLineRunner seedUsersAndGroups(IdentityService identityService) {
-        return args -> {
+    JavaDelegate delegate() {
+        return delegate -> System.out.println("processInstanceId=" + delegate.getProcessInstanceId() + ".");
+    }
 
+    @Bean
+    CommandLineRunner seedUsersAndGroups(RuntimeService runtimeService,
+                                         RepositoryService repositoryService,
+                                         IdentityService identityService) {
+        return args -> {
+            // install groups & users
             Group group = identityService.newGroup("user");
             group.setName("users");
             group.setType("security-role");
@@ -38,11 +53,16 @@ public class Application {
 
             identityService.createMembership("jbarrez", "user");
             identityService.createMembership("jlong", "user");
+
+            // launch a process definition
+            String waiterProcessDefinition = "waiter";
+            Map<String, Object> vars = new HashMap<String, Object>();
+            vars.put("photoId", 232);
+            runtimeService.startProcessInstanceByKey(waiterProcessDefinition, vars);
         };
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
--- demo/src/test/java/demo/ApplicationTests.java
+++ demo/src/test/java/demo/ApplicationTests.java
@@ -1,5 +1,5 @@
 package demo;
-
+/*
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Date;
@@ -19,14 +19,13 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.image.ProcessDiagramGenerator;
 import org.activiti.image.impl.DefaultProcessDiagramGenerator;
-import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-/*
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
