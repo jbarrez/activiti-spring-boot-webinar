9687701156e16ce2412c8e1ea3a67ccc8f502374
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-09-23 00:01:52 -0700

    cleanup

--- demo/src/main/java/demo/Application.java
+++ demo/src/main/java/demo/Application.java
@@ -1,70 +1,108 @@
 package demo;
 
-import org.activiti.engine.IdentityService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.delegate.JavaDelegate;
-import org.activiti.engine.identity.Group;
-import org.activiti.engine.identity.User;
 import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.h2.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/**
- * @author Josh Long
- * @author Joram Barrez
- */
-@ComponentScan
 @Configuration
+@ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
-    @Bean
-    JavaDelegate delegate() {
-        return delegate -> System.out.println("processInstanceId=" + delegate.getProcessInstanceId() + ".");
+/*    @Configuration
+    public static class SimpleMvcConfiguration
+            extends WebMvcConfigurerAdapter {
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/upload.php").setViewName("upload");
+        }
+    }*/
+
+/*
+    @Configuration
+    static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .antMatcher("/api*/
+
+    /**
+     * ")
+     * .authorizeRequests()
+     * .anyRequest().authenticated()
+     * .and()
+     * .httpBasic();
+     * }
+     * }
+     */
+    InputStream bytes(String fn) {
+        try {
+            FileInputStream fileInputStream =
+                    new FileInputStream(new File(String.format("/Users/jlong/Desktop/%s", fn)));
+            return fileInputStream;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Bean
-    CommandLineRunner seedUsersAndGroups(IdentityService identityService, PhotoService photoService) {
+      CommandLineRunner photoProcess(PhotoService photoService, RuntimeService runtimeService, TaskService taskService) {
         return args -> {
-            // install groups & users
-            Group group = identityService.newGroup("user");
-            group.setName("users");
-            group.setType("security-role");
-            identityService.saveGroup(group);
 
-            User joram = identityService.newUser("jbarrez");
-            joram.setFirstName("Joram");
-            joram.setLastName("Barrez");
-            joram.setPassword("password");
-            identityService.saveUser(joram);
+            Long userId = 242L;
+            List<Photo> photos = Arrays.asList("2.jpg", "1.jpg").stream()
+                    .map(pn -> photoService.createPhoto(userId, bytes(pn)))
+                    .collect(Collectors.toList());
 
-            User josh = identityService.newUser("jlong");
-            josh.setFirstName("Josh");
-            josh.setLastName("Long");
-            josh.setPassword("password");
-            identityService.saveUser(josh);
+            ProcessInstance processInstance = photoService.launchPhotoProcess(photos);
+            System.out.println("launched process " + processInstance.getProcessDefinitionId() + '.');
 
-            identityService.createMembership("jbarrez", "user");
-            identityService.createMembership("jlong", "user");
+            photos.forEach(p -> {
+                Execution execution = runtimeService.createExecutionQuery()
+                        .processInstanceId(processInstance.getId())
+                        .activityId("wait")
+                        .variableValueEquals(p).singleResult();
+                runtimeService.signal(execution.getId());
+            });
 
-            photoService.launchPhotoProcess("one", "two", "three");
+            Task task = taskService.createTaskQuery()
+                    .processInstanceId(processInstance.getId())
+                    .taskCandidateGroup("photoReviewers").singleResult();
+            System.out.println("Task name = " + task.getName());
+
+            ((List<Photo>)taskService.getVariable(task.getId(), "photos")).forEach( p -> System.out.println( "Reviewing photo#" + p.getId() + ". "));
+
+            taskService.complete(task.getId(), Collections.singletonMap("approved", true));
+
+
         };
     }
 
@@ -73,76 +111,132 @@ public class Application {
     }
 }
 
-
-@Service
-@Transactional
-class PhotoService {
-
-    private final RuntimeService runtimeService;
-    private final TaskService taskService;
-    private final PhotoRepository photoRepository;
-
-    @Autowired
-    public PhotoService(RuntimeService runtimeService, TaskService taskService, PhotoRepository photoRepository) {
-        this.runtimeService = runtimeService;
-        this.taskService = taskService;
-        this.photoRepository = photoRepository;
-    }
-
-    public void processPhoto(Long photoId) {
-        System.out.println("processing photo#" + photoId);
-    }
-
-    public void launchPhotoProcess(String... photoLabels) {
-        List<Photo> photos = Arrays.asList(photoLabels).stream()
-                .map(Photo::new)
-                .collect(Collectors.toList())
-                .stream().map(photoRepository::save).collect(Collectors.toList());
-
-        Map<String, Object> procVars = new HashMap<>();
-        procVars.put("photos", photos);
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("dogeProcess", procVars);
-
-        List<Execution> waitingExecutions = runtimeService.createExecutionQuery().activityId("wait").list();
-        System.out.println("--> # executions = " + waitingExecutions.size());
-
-        for (Execution execution : waitingExecutions) {
-            runtimeService.signal(execution.getId());
-        }
-
-        Task reviewTask = taskService.createTaskQuery().processInstanceId(processInstance.getId()).singleResult();
-        taskService.complete(reviewTask.getId(), Collections.singletonMap("approved", true));
-
-        long count = runtimeService.createProcessInstanceQuery().count();
-        System.out.println("Proc count " + count);
-
-    }
-}
-
 interface PhotoRepository extends JpaRepository<Photo, Long> {
 }
 
 @Entity
 class Photo {
-
     @Id
     @GeneratedValue
     private Long id;
+    private String userId;
+    private boolean processed;
 
-    Photo() {
+    public boolean isProcessed() {
+        return processed;
     }
-
-    Photo(String username) {
-        this.username = username;
-    }
-
-    private String username;
 
     public Long getId() {
         return id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUserId() {
+        return userId;
     }
+
+    // jpa
+    Photo() {
+    }
+
+    public Photo(String userId) {
+        this.userId = userId;
+    }
+
+    public Photo(String userId, boolean processed) {
+
+        this.userId = userId;
+        this.processed = processed;
+    }
+}
+
+@Service
+@Transactional
+class PhotoService {
+
+    @PostConstruct
+    public void beforeService() throws Exception {
+        File where = this.uploadDirectory.getFile();
+        Assert.isTrue(where.exists() || where.mkdirs(), "the " + where.getAbsolutePath() + " folder must exist!");
+    }
+
+    @Value("file://#{ systemProperties['user.home']}/Desktop/uploads")
+    private Resource uploadDirectory;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    private String photoProcessKey = "photoProcess";
+
+    private File forPhoto(Photo photo) throws IOException {
+        return new File(this.uploadDirectory.getFile(), Long.toString(photo.getId()));
+    }
+
+    private InputStream readPhoto(Photo photo) throws IOException {
+        return new FileInputStream(this.forPhoto(photo));
+    }
+
+    private void writePhoto(Photo photo, InputStream inputStream) {
+        try {
+            try (InputStream input = inputStream; FileOutputStream output = new FileOutputStream(this.forPhoto(photo))) {
+                IOUtils.copy(input, output);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Photo createPhoto(Long userId, InputStream bytesForImage) {
+        Photo photo = this.photoRepository.save(new Photo(Long.toString(userId), false));
+        writePhoto(photo, bytesForImage);
+        return photo;
+    }
+
+    /// step 1
+    public ProcessInstance launchPhotoProcess(List<Photo> photos) {
+        return runtimeService.startProcessInstanceByKey(
+                this.photoProcessKey, Collections.singletonMap("photos", photos));
+    }
+
+    /// step 2
+    public void processSingleUploadedPhoto(Photo photo) {
+        System.out.println("processing photo#" + photo.getId());
+    }
+
+    /// step 3
+
+}
+
+
+class PhotoUploadForm {
+    private List<MultipartFile> files = new ArrayList<>();
+
+    public List<MultipartFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<MultipartFile> files) {
+        this.files = files;
+    }
+}
+
+@RestController
+class UploadController {
+
+    private final RuntimeService runtimeService;
+
+
+    @Autowired
+    public UploadController(RuntimeService runtimeService) {
+        this.runtimeService = runtimeService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/up")
+    public void upload(MultipartHttpServletRequest request) {
+        Optional.ofNullable(request.getMultiFileMap()).ifPresent(m -> m.forEach((k, v) -> System.out.println(k + '=' + v.toString())));
+    }
+
+
 }
--- demo/src/test/java/demo/ApplicationTests.java
+++ demo/src/test/java/demo/ApplicationTests.java
@@ -1,110 +1,16 @@
 package demo;
-/*
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.activiti.engine.HistoryService;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
-import org.activiti.engine.history.HistoricTaskInstance;
-import org.activiti.engine.impl.util.CollectionUtil;
-import org.activiti.engine.repository.ProcessDefinition;
-import org.activiti.engine.runtime.Execution;
-import org.activiti.engine.runtime.ProcessInstance;
-import org.activiti.engine.task.Task;
-import org.activiti.image.ProcessDiagramGenerator;
-import org.activiti.image.impl.DefaultProcessDiagramGenerator;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
-	
-	@Autowired
-	private RepositoryService repositoryService;
-	
-	@Autowired
-	private RuntimeService runtimeService;
-	
-	@Autowired
-	private TaskService taskService;
-	
-	@Autowired
-	private HistoryService historyService;
-	
-	@Autowired
-	private PhotoRepository photoRepository;
 
 	@Test
 	public void contextLoads() {
-		
-	
-//		Photo photo = new Photo("one");
-//		photoRepository.save(photo);
-//		
-//		Photo photo2 = new Photo("two");
-//		photoRepository.save(photo2);
-//		
-//		Photo photo3 = new Photo("three");
-//		photoRepository.save(photo3);
-//		
-//		List<Photo> photos = Arrays.asList(photo, photo2, photo3);
-		
-		// Check the process definition
-		Assert.assertEquals(1, repositoryService.createProcessDefinitionQuery().count());
-		
-		// send a photo to REST service
-		// Start the process instance
-		Map<String, Object> variables = new HashMap<String, Object>();
-		variables.put("photos", Arrays.asList(1L, 2L, 3L));
-		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("dogeProcess", variables);
-		Assert.assertEquals(1, runtimeService.createProcessInstanceQuery().count());
-		
-		// can this be sent to a Spring Integration processor? 
-		// Since we've got 3 photo's in there, should have 3 wait states (the service task has been mocked)
-		List<Execution> waitingExecutions = runtimeService.createExecutionQuery().activityId("wait").list();
-		Assert.assertEquals(3, waitingExecutions.size());
-		
-		// triggering those (mimicing JMS callback) completed the multi instance, leading a the review user task
-		for (Execution execution : waitingExecutions) {
-			Assert.assertEquals(0, taskService.createTaskQuery().count());
-			runtimeService.signal(execution.getId());
-		}
-		
-		// Now, the review task should exist for the reviewers group
-		Task task = taskService.createTaskQuery().taskCandidateGroup("reviewers").singleResult();
-		Assert.assertNotNull(task);
-		Assert.assertEquals("Review results", task.getName());
-		
-		// Complete the task by setting it to approved
-		taskService.complete(task.getId(), CollectionUtil.singletonMap("approved", true));
-		
-		// process should be ended
-		Assert.assertEquals(0, runtimeService.createProcessInstanceQuery().count());
 	}
-	
-	@Test
-	public void demoHistory() {
-	
-		historyService.createHistoricProcessInstanceQuery().count();
-		
-		historyService.createHistoricProcessInstanceQuery().startedAfter(new Date()).startedBefore(new Date()).list();
-		
-		List<HistoricTaskInstance> historyService.createHistoricTaskInstanceQuery().finished().taskAssignee("jlong").list();
-		
-	}
-	
 
 }
-*/
